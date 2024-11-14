9e498837581fe42507779c502f4f3630ff412439
priyaasgowri <priyarkma@gmail.com>
2024-11-13 19:20:40 -0500

    recent changes

--- src/test/java/org/test/Test.java
+++ src/test/java/org/test/Test.java
@@ -1,8 +1,11 @@
 package org.test;
 
-public class Test {
-	public static void main(String[] args) {
-		
-	}
 
+public class Test {
+	public static void main1(String[] args) {
+		System.out.println("Welcome to projectclass");
+	}
 }
+		
+			
+		
